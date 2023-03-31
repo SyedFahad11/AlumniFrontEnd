972622d2e9362bf9926da2d27189b98567423b94
SyedFahad11 <SYEDHU987@GMAIL.COM>
2023-03-31 20:00:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Signincomp from "./Components/Signincomp";
+import HomePage from "./HomePage";
+import Page3 from "./Page3";
+import Page4 from "./Page4";
+import Page5 from "./Page5";
+import Page6 from "./Page6";
+import Page7 from "./Components/Page7";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <Routes>
+        <>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Signincomp" element={<Signincomp />} />
+          <Route path="/Page3" element={<Page3 />} />
+          <Route path="/Page4" element={<Page4 />} />
+          <Route path="/Page5" element={<Page5 />} />
+          <Route path="/Page6" element={<Page6 />}  />
+          <Route path="/Page7" element={<Page7 />}  />
+                                </>
+      </Routes>
     </div>
   );
 }
