0e381636b21c5f5a16573a915486098fd17021a8
SyedFahad11 <SYEDHU987@GMAIL.COM>
2023-03-31 20:18:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
-import Signincomp from "./Components/Signincomp";
-import HomePage from "./HomePage";
-import Page3 from "./Page3";
-import Page4 from "./Page4";
-import Page5 from "./Page5";
-import Page6 from "./Page6";
-import Page7 from "./Components/Page7";
+import SignUp from "./Components/LandingPages/Register";
+import LandingPage from "./Components/LandingPages/Landing";
+import SignIn from "./Components/LandingPages/SignIn";
+
+import AlumniLanding from "./Components/Alumni/Landing";
+import UpdateProfile from "./Components/Alumni/UpdateProfile";
+import Events from "./Components/Alumni/Events&Notice";
+import Profile from "./Components/Alumni/ViewProfile";
+
+import CollegeLanding from "./Components/College/Landing"
+import CollegeEvents from "./Components/College/Events&Notice"
+import NewRequests from "./Components/College/NewRequests";
+import CreateEvents from "./Components/College/CreateEvents";
+import CreateNotices from "./Components/College/CreateNotices"
+
+import CompanyLanding from "./Components/Company/Landing";
+import ViewProfile from "./Components/Company/ViewProfile";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,13 +24,23 @@ function App() {
     <div class="App">
       <Routes>
         <>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Signincomp" element={<Signincomp />} />
-          <Route path="/Page3" element={<Page3 />} />
-          <Route path="/Page4" element={<Page4 />} />
-          <Route path="/Page5" element={<Page5 />} />
-          <Route path="/Page6" element={<Page6 />}  />
-          <Route path="/Page7" element={<Page7 />}  />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Register" element={<SignUp />} />
+        <Route path="/SignIn"  element={<SignIn/>} />
+
+        <Route path="/alumni" element={< AlumniLanding />} />  
+        <Route path="/UpdateProfile" element={<UpdateProfile />} />
+        <Route path="/Profile" element={<Profile />}  />
+        <Route path="/Events" element={<Events />} />
+
+        <Route path="/college" element={<CollegeLanding />}  /> 
+        <Route path="/collegeEvents" element={<CollegeEvents />}  />  
+        <Route path="/NewRequests" element={<NewRequests />}  />
+        <Route path="/CreateEvents" element={<CreateEvents/>}   />
+        <Route path="/CreateNotices" element={<CreateNotices/>}   />
+          
+          <Route path="/company" element={<CompanyLanding/>}   />
+          <Route path="/company/profile" element={<ViewProfile/>} />
                                 </>
       </Routes>
     </div>
